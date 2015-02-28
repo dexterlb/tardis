21271ca80fa77e34e464be9998bcd2b944516e21
Angel Angelov <hextwoa@gmail.com>
2015-02-28 11:19:01 +0200

    add nokogiri

--- dns.rb
+++ dns.rb
@@ -1,7 +1,24 @@
 #!/usr/bin/env ruby
 
-class DnsMonitor
+require 'open-uri'
+require 'nokogiri'
 
+class DnsMonitor
+  def initialize(router_host: 'router',
+                 router_user: 'admin',
+                 router_password: 'password')
+    @router_host = router_host
+    @router_user = router_user
+    @router_password = router_password
+  end
+
+  def router_url
+    "http://#{@router_user}:#{@router_password}@#{@router_host}"
+  end
+
+  def get_data
+    html = Nokogiri::HTML(open(router_url + '/DHCPTable.htm'))
+  end
 end
 
 # vim: set shiftwidth=2:
