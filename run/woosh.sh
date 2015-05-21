c611a2cc1fe4ed963847b116f5744232a3fa6b08
Angel Angelov <hextwoa@gmail.com>
2015-05-21 13:07:11 +0300

    now using sox for playing audio

--- run/woosh.sh
+++ run/woosh.sh
@@ -5,8 +5,8 @@ cd "$(dirname "$(readlink -f "$0")")"  # go to the dir of the script
 trap "amp_power off" SIGINT SIGTERM
 amp_power on
 case "$1" in
-    1) ogg123 media/woosh1.ogg &> /dev/null ;;
-    2) ogg123 media/woosh2.ogg &> /dev/null ;;
-    *) ogg123 media/woosh0.ogg &> /dev/null ;;
+    1) play media/woosh1.ogg &> /dev/null ;;
+    2) play media/woosh2.ogg &> /dev/null ;;
+    *) play media/woosh0.ogg &> /dev/null ;;
 esac
 amp_power off
