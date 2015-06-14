0e561e4b1169029c9295dc6c1963e38449cf721b
Angel Angelov <hextwoa@gmail.com>
2015-06-14 13:24:03 +0300

    use hwclock

--- run/ntp.sh
+++ run/ntp.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
+echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-0/new_device
+hwclock -s
+
 trap "exit" INT
 /usr/bin/ntpd -gq
 sleep 60
 while true; do
     /usr/bin/ntpd -gq
+    hwclock -w
     sleep 1600
 done
