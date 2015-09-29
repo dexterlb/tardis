6caa8d24dbfc246d7049b068de7b78f808221e2b
Angel Angelov <hextwoa@gmail.com>
2015-09-29 17:11:27 +0300

    simplify ip.sh

--- run/ip.sh
+++ run/ip.sh
@@ -1,73 +1,35 @@
 #!/bin/zsh
 dir="$(dirname "${0}")"
-. "${dir}/common.sh"
 . "${dir}/ip_config.sh"
-if [[ -z $(fmdate '%Y%m%d') ]]; then
-    # assertion failed
-    exit 1
-fi
 
-logfile="/home/human/log/ip/$(fmdate '%Y%m%d').log"
 match_ok='^ERROR\: Address ([0-9.]+) has not changed\.'
 match_new='Updated [0-9]+ host\(s\) .+ to ([0-9.]+) in [0-9]+\.[0-9]+ seconds'
 
-function lag {
-    pinfo "lag.${1}" && return
-    ping "${1}" -c 1 -w 20 | tail -1 \
-        | perl -ne 'm/.+ = ([0-9.]+)\/.*/ ; print "$1\n"' | read t
-    echo ${pipestatus} | read stat garb
-    if [[ ! ${stat} -eq 0 ]]; then
-        t="-${stat}"
-    fi
-    echo "${t}"
-}
-
-function dl {
-    pinfo "time ip status" && return
-    t1=$(date +'%s%N')
+function update {
     curl -q -m20 "${url}" 2>/dev/null | read out
-    echo ${pipestatus} | read stat garb
-    t2=$(date +'%s%N')
-    t=$(( (t2 - t1)/1000000 ))
+    echo ${pipestatus} | read response_status _
 
-    if [[ ${stat} -eq 0 ]]; then
-        if [[ "${out}" =~ "${match_ok}" ]]
-        then
-            echo "${t} ${match} ok"
-        elif [[ "${out}" =~ "${match_new}" ]]
-        then
-            echo "${t} ${match} new"
+    if [[ ${response_status} -eq 0 ]]; then
+        if [[ "${out}" =~ "${match_ok}" ]]; then
+            echo "${match} ok"
+        elif [[ "${out}" =~ "${match_new}" ]]; then
+            echo "${match} new"
         else
-            echo "${t} ? out: ${out}"
+            echo "server error: ${out}"
         fi
     else
-        echo "-1 ? err.${stat}"
+        echo "error: #${stat}"
     fi
-}
-
-function report {
-    echo "$(timestamp) $(lag 8.8.8.8) $(dl)"
 }
 
 case "${1}" in
-    'log')
-        report | savelog "${logfile}"
-        ;;
-    'logloop')
-        trap "exit" INT
+    loop)
         while true; do
-            report | savelog "${logfile}"
-            sleep 300
+            update > /tmp/last_ip
+            sleep 300 || break
         done
         ;;
-    'report')
-        report
-        ;;
-    'info')
-        printinfo=42
-        report
-        ;;
-    *)
-        echo 'First argument must be (log|report|info)'
+    update)
+        update
         ;;
 esac
