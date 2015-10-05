648d46b0bd79be54d3d038e7448fd805f2202979
Angel Angelov <hextwoa@gmail.com>
2015-10-05 13:30:18 +0300

    rename tardis_bot

--- run/core/tardis_telegram_bot.py
+++ run/core/tardis_telegram_bot.py
@@ -5,27 +5,30 @@ import logging
 
 import telegram
 
-class TardisBot:
+
+class TardisTelegramBot:
     def __init__(self, telegram_token):
         self.bot = telegram.Bot(token=telegram_token)
+        self.logger = logging.getLogger('tardis_telegram_bot')
 
     def process_message(self, message):
         self.bot.sendMessage(chat_id=message.chat_id,
                              text=('you said: ' + message.text))
+
     def loop(self):
         last_update = 0
         while True:
             for update in self.bot.getUpdates(offset=last_update, timeout=30):
                 if update.message:
-                    logging.info('received update with message: %s',
+                    self.logger.info('received update with message: %s',
                                  str(update))
                     self.process_message(update.message)
                 else:
-                    logging.warning('received update without message: %s',
+                    self.logger.warning('received update without message: %s',
                                     str(update))
 
                 last_update = update.update_id + 1
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
-    TardisBot(sys.argv[1]).loop()
+    logging.basicConfig(level=logging.INFO)
+    TardisTelegramBot(sys.argv[1]).loop()
