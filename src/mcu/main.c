cd28277dfe952c12453c32a592e6d9fbd2073139
Angel Angelov <hextwoa@gmail.com>
2015-11-25 21:09:13 +0200

    add testing firmware

--- src/mcu/main.c
+++ src/mcu/main.c
@@ -1,7 +1,13 @@
+#include <avr/io.h>
+
+#define F_CPU 12000000ul
+
+#include <util/delay.h>
+
 static inline void init(void)
 // main init
 {
-
+    DDRD = 0x00;    // all outputs
 }
 
 int main(void)
@@ -10,6 +16,10 @@ int main(void)
 
     // main loop
     for (;;) {
+        PORTD = 0xFF;
+        _delay_us(1000000);
+        PORTD = 0x00;
+        _delay_us(1000000);
     }
     return 0;
 }
