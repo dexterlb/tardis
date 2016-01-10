d8ef1772f0ffe75e1373d3165390182f49aa3436
Angel Angelov <hextwoa@gmail.com>
2016-01-11 01:41:50 +0200

    make code more human-readable

--- src/go/spi_check/spi_check.go
+++ src/go/spi_check/spi_check.go
@@ -1,43 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/luismesas/goPi/spi"
 )
 
-func main() {
+func initSpi() (*spi.SPIDevice, error) {
 	spiDevice := spi.NewSPIDevice(0, 0)
 	err := spiDevice.Open()
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
 
 	err = spiDevice.SetMode(0)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
 
 	err = spiDevice.SetBitsPerWord(8)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
 
 	err = spiDevice.SetSpeed(5000)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	return spiDevice, nil
+}
+
+func main() {
+	spiDevice, err := initSpi()
+	if err != nil {
+		log.Print(err)
 		return
 	}
 
 	data, err := spiDevice.Send([]byte{1, 2, 3, 4})
-
 	if err != nil {
 		log.Print(err)
-	} else {
-		fmt.Printf("data: %v\n", data)
+		return
 	}
+
+	log.Printf("got data: %v\n", data)
 }
