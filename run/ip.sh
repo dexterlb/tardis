e91ae44c7d6f27c067e1496556ca7c53679dc24f
Angel Angelov <hextwoa@gmail.com>
2017-03-26 21:05:54 +0300

    use namecheap for IP updating

--- run/ip.sh
+++ run/ip.sh
@@ -2,31 +2,22 @@
 dir="$(dirname "${0}")"
 . "${dir}/ip_config.sh"
 
-match_ok='^ERROR\: Address ([0-9.]+) has not changed\.'
-match_new='Updated [0-9]+ host\(s\) .+ to ([0-9.]+) in [0-9]+\.[0-9]+ seconds'
+function update_single {
+    curl -s -m20 "https://dynamicdns.park-your-domain.com/update?host=${1}&domain=${domain}&password=${password}" \
+        | grep -vF '<ErrCount>0</ErrCount>'
+}
 
 function update {
-    curl -q -m20 "${url}" 2>/dev/null | read out
-    echo ${pipestatus} | read response_status _
-
-    if [[ ${response_status} -eq 0 ]]; then
-        if [[ "${out}" =~ "${match_ok}" ]]; then
-            echo "${match} ok"
-        elif [[ "${out}" =~ "${match_new}" ]]; then
-            echo "${match} new"
-        else
-            echo "server error: ${out}"
-        fi
-    else
-        echo "error: #${stat}"
-    fi
+    for subdomain in ${subdomains}; do
+        update_single "${subdomain}"
+    done
 }
 
 case "${1}" in
     loop)
         while true; do
-            update > /tmp/last_ip
-            sleep 300 || break
+            update
+            sleep "${interval}" || break
         done
         ;;
     update)
