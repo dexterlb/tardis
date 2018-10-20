303d0c0238f7bc9dcd61d61bdd51251887ae10a7
dexterlb <dexterlb@qtrp.org>
2018-10-20 19:40:33 +0300

    fix dependencies

--- src/control/mix.exs
+++ src/control/mix.exs
@@ -14,7 +14,7 @@ defmodule Control.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :server, :global_registry],
+      extra_applications: [:logger, :server, :global_registry, :mqtt_mesh],
       mod: {Control.Application, []}
     ]
   end
