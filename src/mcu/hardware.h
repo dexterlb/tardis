cb003354742065a85fbf695493653bcaa64e56f6
dexterlb <dexterlb@qtrp.org>
2019-12-26 19:20:54 +0200

    simple uart communication

--- src/mcu/hardware.h
+++ src/mcu/hardware.h
@@ -1,17 +1,9 @@
 #pragma once
 
 #define F_CPU 12000000ull
+#define BAUDRATE 19200
 
 // 1 - out, 0 - in   v bits v
 //                   76543210
-#define DDRB_STATE 0b01000000
-#define DDRD_STATE 0b11110100
-
-#define MISO_PORT PORTB
-#define MISO_PIN 6
-
-#define MOSI_IN PINB
-#define MOSI_PIN 5
-
-#define SCK_IN PINB
-#define SCK_PIN 7
+#define DDRB_STATE 0b00001111
+#define DDRD_STATE 0b00010110
--- src/mcu/main.c
+++ src/mcu/main.c
@@ -4,46 +4,73 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#include "simple_uart.h"
 #include "bit_operations.h"
 
+ISR(USART_RX_vect) {
+    char x = uart_read_byte();
 
-void spi_init() {
-    // we are clocked externally by the master
-    USICR = (1 << USIWM0) | (1 << USICS1);
+    if (x == 'a') {
+        setbitval(PORTB, 1, bitclear(PORTB, 1));
+    }
+    if (x == 's') {
+        setbitval(PORTB, 2, bitclear(PORTB, 2));
+    }
+    if (x == 'd') {
+        setbitval(PORTB, 0, bitclear(PORTB, 0));
+    }
+    if (x == 'f') {
+        setbitval(PORTB, 3, bitclear(PORTB, 3));
+    }
+    char bla[] = "[.]";
+    bla[1] = x;
+    uart_write_string(bla);
 }
 
-uint8_t spi_readWrite(uint8_t data)
-{
-    while (bitset(PIND, 3));    // we are a slave, so wait for Slave Select
-    USIDR = data;
-    setbit(USISR, USIOIF);
-    while (bitclear(USISR, USIOIF));
-    return USIDR;
+const uint16_t pwm_top = 0xffff;
+
+static inline void timer1_init(void) {
+    // use OC1A for fast PWM, no prescaler
+    TCCR1A = (1 << COM1A0) | (1 << COM1A1) |
+             (0 << COM1B0) | (0 << COM1B1) |
+             (0 << WGM10)  | (1 << WGM11);
+
+    TCCR1B = (1 << WGM12) | (1 << WGM13) |
+             (1 << CS10)  | (0 << CS11)  | (0 << CS12);
+
+    // Top value
+    ICR1 = pwm_top;
+
+    // PWM value
+    OCR1A = 0xfffe;
 }
 
 static inline void init(void)
-// main init
 {
     DDRB = DDRB_STATE;
     DDRD = DDRD_STATE;
 
-    spi_init();
-	// sei();
+    timer1_init();
+    uart_init();
+    uart_enable_interrupt();
+	sei();
 }
 
 
 int main(void)
 {
     init();
-    PORTD = 0;
-    // main loop
-    uint8_t data = 42;
+
+    for (int i = 0; i < 3; i++) {
+        _delay_ms(500);
+        setbit(PORTB, 1);
+        _delay_ms(500);
+        clearbit(PORTB, 1);
+    }
 
     for (;;) {
-        data = spi_readWrite(data * 2);
-        if (data) {
-            PORTD = data;
-        }
+        _delay_ms(1);
+        OCR1A = OCR1A + 10;
     }
     return 0;
 }
